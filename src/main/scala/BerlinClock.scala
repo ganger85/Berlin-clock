ddf2df2894f79ec46c93b9f1e16170d289c05f3f
arinconstrio <arincon@stratio.com>
2015-03-04 17:29:44 +0100

    top Minutes lamp

--- src/main/scala/BerlinClock.scala
+++ src/main/scala/BerlinClock.scala
@@ -1,23 +1,27 @@
 
 object BerlinClock {
+  def topMinutes(i: Int) = generateTop(i, 11)
 
-  def generateTop(j: Int): String = {
-    var l: String = ""
-    for (ii <- 0 until 4)
-      if (ii < j) l += "R" else l += "0"
-    l
-  }
+
+  def bottomHours(i: Int) = generateTop(i % 5, 4)
 
   def topHours(i: Int): String = {
     val number = (i - (i % 5)) / 5
-    val l = generateTop(number)
+    val l = generateTop(number, 4)
     l
   }
-
 
   def seconds(number: Int) = number % 2 match {
     case (0) => "Y"
     case _ => "O"
   }
+
+  def generateTop(j: Int, total: Int): String = {
+    var l: String = ""
+    for (ii <- 0 until total)
+      if (ii < j) l += "R" else l += "0"
+    l
+  }
+
 
 }
--- src/test/scala/ClockTest.scala
+++ src/test/scala/ClockTest.scala
@@ -14,4 +14,19 @@ class ClockTest extends  UnitSpec{
     BerlinClock.topHours(23) should be("RRRR")
     BerlinClock.topHours(24) should be("RRRR")
   }
+  "Bottom hours" should "have 4 lamps" in {
+    BerlinClock.bottomHours(5).length should be(4)
+  }
+
+  it should "light a red lamp for every hour left from top hours" in {
+    BerlinClock.bottomHours(0) should be("0000")
+    BerlinClock.bottomHours(13) should be("RRR0")
+    BerlinClock.bottomHours(23) should be("RRR0")
+    BerlinClock.bottomHours(24) should be("RRRR")
+  }
+
+  "Top minutes" should "have 11 lamps" in {
+    BerlinClock.topMinutes(34).length should be(11)
+  }
+
   }
